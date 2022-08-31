8427a0eb299f9ffceae9bb617111a4ac5a2152ac
+ <+>
2022-09-01 01:19:10 +0300

    First project

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-
-fetch('https://fakestoreapi.com/products/1')
-.then(res=>res.json())
-.then(json=>console.log(json))
+//const title = document.getElementsByClassName('table-dark')
 
 
+fetch('https://fakestoreapi.com/products').then ((data)=> {
+        return data.json();
+}).then((objectData) =>{
+        console.log(objectData[0].title);
+        let tableData = "";
+        objectData.map((values)=>{
+                //tableData+=`<h1>${values.title}</h1>`;
+                tableData+=`<tr>
+                <td>${values.title}</td>
+                <td>${values.description}</td>
+                <td>${values.price}</td>
+                <td><img src="${values.image}"/></td>
+              </tr>`;
+        });
+        document.getElementById("table-body").innerHTML=tableData;
+        }).catch((err)=>{
+                console.log(error);
+        })
 
+
+            
